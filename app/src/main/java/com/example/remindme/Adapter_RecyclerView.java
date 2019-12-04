8c6f031753da7cadcad7caa72044d3425dd0f9cf
M.Abdullah Sultan <abdullahsultan448@gmail.com>
2019-12-03 17:35:08 -0800

    Going for 2nd activity adapter done

--- app/src/main/java/com/example/remindme/Adapter_RecyclerView.java
+++ app/src/main/java/com/example/remindme/Adapter_RecyclerView.java
@@ -1,4 +1,61 @@
 package com.example.remindme;
 
-public class Adapter_RecyclerView extends {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Adapter_RecyclerView extends RecyclerView.Adapter<Adapter_RecyclerView.My_Holder> {
+
+    private Context context;
+    private int layout;
+    private ArrayList<Time_Setter> data;
+
+    public Adapter_RecyclerView(Context context, int layout, ArrayList<Time_Setter> data) {
+        this.context = context;
+        this.layout = layout;
+        this.data = data;
+    }
+
+
+
+    @NonNull
+    @Override
+    public My_Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(context).inflate(layout,parent,false);
+            My_Holder holder = new My_Holder(view);
+            return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull My_Holder holder, int position) {
+        Time_Setter time_setter =data.get(position);
+        holder.textView_reminderName.setText(time_setter.remind_text);
+        holder.textView_TimeRemaining.setText(time_setter.time_remaining_text);
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public class  My_Holder extends RecyclerView.ViewHolder{
+        private TextView textView_reminderName;
+        private TextView textView_TimeRemaining;
+
+        public My_Holder(@NonNull View itemView) {
+            super(itemView);
+            textView_reminderName = itemView.findViewById(R.id.textView_Reminder);
+            textView_TimeRemaining = itemView.findViewById(R.id.textView_remainingTime);
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/remindme/Time_Setter.java
@@ -0,0 +1,16 @@
+package com.example.remindme;
+
+import android.widget.ImageView;
+
+public class Time_Setter {
+
+    public String remind_text;
+    public String time_remaining_text;
+    public ImageView imageView;
+
+    public Time_Setter(String remind_text, String time_remaining_text, ImageView imageView) {
+        this.remind_text = remind_text;
+        this.time_remaining_text = time_remaining_text;
+        this.imageView = imageView;
+    }
+}
