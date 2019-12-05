50d1de872190037bfcd407d5b02013eb3f8a5c3d
M.Abdullah Sultan <abdullahsultan448@gmail.com>
2019-12-04 22:15:33 -0800

    Date and Time Pickers done

--- app/src/main/java/com/example/remindme/Activity_Time_Setting.java
+++ app/src/main/java/com/example/remindme/Activity_Time_Setting.java
@@ -1,5 +1,6 @@
 package com.example.remindme;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -14,7 +15,7 @@ public class 	Activity_Time_Setting extends AppCompatActivity  {
     private TextView txtDate, txtTme;
     private DatePicker datePicker;
     private TimePicker timePicker;
-    private int mYear, mMonth, mDay, mHour, mMinute;
+    private int year, month, day, hour, minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +32,83 @@ public class 	Activity_Time_Setting extends AppCompatActivity  {
 
         datePicker=findViewById(R.id.datePicker);
         timePicker=findViewById(R.id.timePicker);
+
+        btnDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePicker.setVisibility(View.VISIBLE);
+                btnOK_datePick.setVisibility(View.VISIBLE);
+
+
+                btnTimePicker.setVisibility(View.GONE);
+                btnDatePicker.setVisibility(View.GONE);
+                btn_ok_timePick.setVisibility(View.GONE);
+                txtDate.setVisibility(View.GONE);
+                txtTme.setVisibility(View.GONE);
+                timePicker.setVisibility(View.GONE);
+            }
+        });
+
+        btnOK_datePick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                year = datePicker.getYear();
+                month = datePicker.getMonth();
+                day = datePicker.getDayOfMonth();
+                String date = month + " / " + day + " / " + year;
+                txtDate.setText(date);
+
+                datePicker.setVisibility(View.GONE);
+                btnOK_datePick.setVisibility(View.GONE);
+
+
+                btnTimePicker.setVisibility(View.VISIBLE);
+                btnDatePicker.setVisibility(View.VISIBLE);
+                btn_ok_timePick.setVisibility(View.GONE);
+                txtDate.setVisibility(View.VISIBLE);
+                txtTme.setVisibility(View.VISIBLE);
+                timePicker.setVisibility(View.GONE);
+
+            }
+        });
+
+        btnTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timePicker.setVisibility(View.VISIBLE);
+                btn_ok_timePick.setVisibility(View.VISIBLE);
+
+                btnTimePicker.setVisibility(View.GONE);
+                btnDatePicker.setVisibility(View.GONE);
+                datePicker.setVisibility(View.GONE);
+                txtDate.setVisibility(View.GONE);
+                txtTme.setVisibility(View.GONE);
+                btnOK_datePick.setVisibility(View.GONE);
+            }
+        });
+
+        btn_ok_timePick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hour = timePicker.getHour();
+                minute = timePicker.getMinute();
+
+                String time = hour + " : " + minute;
+                txtTme.setText(time);
+
+                datePicker.setVisibility(View.GONE);
+                btnOK_datePick.setVisibility(View.GONE);
+                btnTimePicker.setVisibility(View.VISIBLE);
+                btnDatePicker.setVisibility(View.VISIBLE);
+                btn_ok_timePick.setVisibility(View.GONE);
+                txtDate.setVisibility(View.VISIBLE);
+                txtTme.setVisibility(View.VISIBLE);
+                timePicker.setVisibility(View.GONE);
+
+            }
+        });
+
+
 
     }
 
